a0e7881b528931036189ea25bdcd018ba050990f
PedroJorgeGuiamba <pedrojorgeguiamba@gmail.com>
2025-06-04 14:09:38 +0200

    Adicionado conector php

---
+++ conector.php
@@ -0,0 +1,21 @@
+<? php
+    mysqli_report(MYSQLI_REPORT_ERROR);
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "Familiaguiamba1";
+    $dbname = "GestaoDeFundos";
+    $port = 3306;
+
+    $conexao = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+    if ($conexao -> connect_errno) {
+        echo "Conexao MySQL Falhou: " . $mysqli -> connect_error;
+        exit();
+        Jelse{
+            echo "Conexao de PHP com MySQL feito com sucesso
+            usando: ", $conexao->host_info;
+        }
+    }
+
+?>
